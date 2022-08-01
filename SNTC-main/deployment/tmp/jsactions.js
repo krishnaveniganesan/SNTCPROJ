808d29bddb8671a154f258ecfc8b966327a42b28
krishnaveniganesan <89210231+krishnaveniganesan@users.noreply.github.com>
2022-08-01 16:32:53 +0530

    Delete SNTC-main directory

--- SNTC-main/deployment/tmp/jsactions.js
+++
@@ -1,1 +0,0 @@
-export const MyFirstModule$JS_ToggleNavigation = async () => (await import("C:/Users/krish/OneDrive/Documents/Mendix/SNTC-main/javascriptsource/myfirstmodule/actions/JS_ToggleNavigation")).JS_ToggleNavigation;
